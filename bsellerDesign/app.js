322c47512d22a143d5092f510ac97c7bd56bd5e4
smenesesd <smenesesd@eafit.edu.co>
2023-03-17 16:35:17 -0500

    Update design, more functions

--- bsellerDesign/app.js
+++ bsellerDesign/app.js
@@ -1,6 +1,27 @@
 const wrapper = document.querySelector(".sliderWrapper")
 const menuItems = document.querySelectorAll(".menuItem")
 
+const products = [
+    {
+        id:1,
+        title: "Air Force",
+        price: 119,
+        colors: [
+            {
+                code: "black",
+                img: "/img/air.png",
+            },
+            {
+                code: "grey",
+                img: "/img/air2.png"
+            },
+        ],
+    },
+    {
+        
+    }
+]
+
 menuItems.forEach((item,index)=>{
     item.addEventListener("click", ()=>{
         wrapper.style.transform=`translateX(${-100 * index}vw)`;
